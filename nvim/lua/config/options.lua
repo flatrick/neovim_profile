879e8b7e383c9ad184f352859fb1d58a7f8b968e
Patrik Mattsson <57222317+flatrick-priv@users.noreply.github.com>
2024-11-14 19:39:25 +0100

    check what OS is running and change settings accordingly

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -8,10 +8,13 @@ vim.opt.splitright = true
 -- Search settings
 vim.opt.incsearch = true
 
+-- Need to make this conf able to switch between Windows and Linux --
 -- set desired shell
-vim.o.shell = 'pwsh'
-vim.o.shellcmdflag = "-NoProfile -NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';$PSStyle.OutputRendering = [System.Management.Automation.OutputRendering]::PlainText;"
-vim.o.shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-vim.o.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-vim.o.shellquote = ''
-vim.o.shellxquote = ''
+if vim.fn.has('win32') then
+	vim.o.shell = 'pwsh'
+	vim.o.shellcmdflag = "-NoProfile -NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';$PSStyle.OutputRendering = [System.Management.Automation.OutputRendering]::PlainText;"
+	vim.o.shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+	vim.o.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+	vim.o.shellquote = ''
+	vim.o.shellxquote = ''
+end
